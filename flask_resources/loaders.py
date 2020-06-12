fd439bdfba7b68eda5fa70e4510888beb63bc9f3
Pablo Panero <pablo.panero@cern.ch>
2020-06-12 17:10:39 +0200

    loaders: add module file

---
+++ flask_resources/loaders.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Flask-Resources is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Request loader view."""
+
+
+def load_request(self):
+    """Load item from request."""
+    pass
