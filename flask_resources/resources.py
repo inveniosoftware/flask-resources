646e24a8141fb839618d4c53c3fcab8771758282
Pablo Panero <pablo.panero@cern.ch>
2020-06-12 14:02:28 +0200

    context,resources: fix module docstring

--- flask_resources/resources.py
+++ flask_resources/resources.py
@@ -5,7 +5,7 @@
 # Flask-Resources is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Library for easily implementing REST APIs."""
+"""Resource view."""
 
 from flask import Blueprint, abort
 
