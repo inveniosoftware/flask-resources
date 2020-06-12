e59e7279c35ad583cbb48f78225d8d584952711d
Pablo Panero <pablo.panero@cern.ch>
2020-06-12 17:09:40 +0200

    tests: set resource test skeleton

---
+++ tests/test_resources.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Flask--Resources is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Resources test module."""
+
+
+def test_custom_resource():
+    """Test the registration of a custom resource.
+
+    It tests the registration process itself and the correct functioning
+    of its endpoints.
+    """
+    # bp = UserResource(config=ResourceConfig(), datastore).as_blueprint()
+    # app.register_blueprint(bp)
+    pass
