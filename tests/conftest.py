94b57799b6c521fdfce5933e16141456fed3d54e
Pablo Panero <pablo.panero@cern.ch>
2020-06-12 17:09:40 +0200

    tests: set resource test skeleton

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,52 @@ import pytest
 from flask import Flask
 
 
+class CustomSerializer(SerializerMixin):
+    """Custom serializer implementation."""
+
+    def serialize_object(self, object, response_ctx, *args, **kwargs):
+        """Custom object serialization."""
+        pass
+
+    def serialize_object_list(self, object_list, response_ctx, *args, **kwargs):
+        """Custom object list serialization."""
+        pass
+
+
+class CustomResourceConfig:
+    """Custom resource configuration."""
+
+    item_handlers = {"application/json": ItemResponse(CustomSerializer)}
+
+
+class CustomResource(CollectionResource):
+    """Custom resource implementation."""
+
+    def search(self, request_context):
+        """Search."""
+        pass
+
+    def create(self):
+        """Create."""
+        pass
+
+    def read(self, id, *args, **kwargs):
+        """Read."""
+        pass
+
+    def update(self, data, *args, **kwargs):
+        """Update."""
+        pass
+
+    def partial_update(self, data, *args, **kwargs):
+        """Partial update."""
+        pass
+
+    def delete(self, *args, **kwargs):
+        """Delete."""
+        pass
+
+
 @pytest.fixture(scope="module")
 def create_app(instance_path):
     """Application factory fixture."""
@@ -26,3 +72,9 @@ def create_app(instance_path):
         return app_
 
     return app
+
+
+@pytest.fixture(scope="module")
+def custom_resource_config():
+    """Returns a simple custom resource with a custom configuration."""
+    return CustomResource(config=CustomResourceConfig)
