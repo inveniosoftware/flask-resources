351f1767844377e2197a6fc30a6c5ce751e42c64
Pablo Panero <pablo.panero@cern.ch>
2020-06-15 11:56:34 +0200

    tests: add custom resource basic flow test

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -7,6 +7,8 @@
 
 """Resources test module."""
 
+import json
+
 
 def test_custom_resource(base_client):
     """Test the registration of a custom resource.
@@ -14,26 +16,29 @@ def test_custom_resource(base_client):
     It tests the registration process itself and the correct functioning
     of its endpoints.
     """
+    headers = {"content-type": "application/json", "accept": "application/json"}
+
     # The resource returns a list, empty for now
-    resource_obj = base_client.get("/resources/")
+    resource_obj = base_client.get("/resources/", headers=headers)
     assert resource_obj.status_code == 200
-    assert len(resource_obj.get_json) == 0
+    assert len(resource_obj.get_json()) == 0
 
     # Create a resource object
-    obj_content = {"id": "1234-ABDC", "content": "test resource obj content"}
-    resource_obj = base_client.post("/resources/", body=obj_content)
+    obj_content = {"id": "1234-ABCD", "content": "test resource obj content"}
+    obj_json = json.dumps(obj_content)
+    resource_obj = base_client.post("/resources/", data=obj_json, headers=headers)
     assert resource_obj.status_code == 201
 
     # Search for the previously created obj
-    resource_obj = base_client.get("/resources/")
+    resource_obj = base_client.get("/resources/", headers=headers)
     assert resource_obj.status_code == 200
 
     resource_obj_json = resource_obj.get_json()
-    assert len(resource_obj.data) == 1
+    assert len(resource_obj.get_json()) == 1
     assert resource_obj_json[0]["id"] == "1234-ABCD"
 
     # Get the previously created obj
-    resource_obj = base_client.get("/resources/1234-ABDC")
+    resource_obj = base_client.get("/resources/1234-ABCD", headers=headers)
     assert resource_obj.status_code == 200
 
     resource_obj_json = resource_obj.get_json()
