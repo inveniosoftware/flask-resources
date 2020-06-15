4c23d9097328892b1e9ff83ebc3c629450e2df5b
Pablo Panero <pablo.panero@cern.ch>
2020-06-15 11:49:03 +0200

    serializers: make response context optional

--- flask_resources/serializers/serializers.py
+++ flask_resources/serializers/serializers.py
@@ -38,20 +38,20 @@ class SerializableMixin:
 class SerializerMixin:
     """Serializer Interface."""
 
-    def serialize_object(self, object, response_ctx, *args, **kwargs):
+    def serialize_object(self, object, response_ctx=None, *args, **kwargs):
         """Serialize a single object according to the response ctx.
 
         The object type must implement ``SerializableMixin``.
         """
         raise NotImplementedError()
 
-    def serialize_object_list(self, object_list, response_ctx, *args, **kwargs):
+    def serialize_object_list(self, object_list, response_ctx=None, *args, **kwargs):
         """Serialize a list of objects according to the response ctx.
 
         Each object type of the list should implement ``SerializableMixin``.
         """
         raise NotImplementedError()
 
-    def serialize_error(self, error, response_ctx, *args, **kwargs):
+    def serialize_error(self, error, response_ctx=None, *args, **kwargs):
         """Serialize an error reponse according to the response ctx."""
         raise NotImplementedError()
