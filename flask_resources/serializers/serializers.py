7003157a4842a810ee12c8e6c9e0484dd9b3d909
Pablo Panero <pablo.panero@cern.ch>
2020-06-15 10:09:14 +0200

    resoureces: add loaders to config and remove to-dos

--- flask_resources/serializers/serializers.py
+++ flask_resources/serializers/serializers.py
@@ -5,7 +5,7 @@
 # Flask-Resources is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Serializers and required interfaces."""
+"""Serializers required interfaces."""
 
 
 class SerializableMixin:
