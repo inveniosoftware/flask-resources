19109d41fafd7159f9bf95ee249a11b784f032d1
Guillaume Viger <fenekku@fenekku.com>
2020-07-07 14:52:23 -0500

    copyright: add Northwestern University to a couple

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
 #
-# Flask--Resources is free software; you can redistribute it and/or modify
+# Flask-Resources is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Resources test module."""
