a84401db73d81339bfcc9e9cd6f4ab19cc779dc2
Pablo Panero <pablo.panero@cern.ch>
2020-11-02 08:53:42 +0100

    workflows: add tests and remove travis file

--- run-tests.sh
+++ run-tests.sh
@@ -6,9 +6,15 @@
 # Flask-Resources is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
 pydocstyle flask_resources tests docs && \
 isort flask_resources tests --multi-line=3 --line-width=88 --trailing-comma --force-grid-wrap=0 --use-parentheses --check-only --diff && \
 black --check --diff flask_resources tests && \
-check-manifest --ignore ".travis-*" && \
+check-manifest --ignore ".*-requirements.txt" && \
 sphinx-build -qnNW docs docs/_build/html && \
 pytest
