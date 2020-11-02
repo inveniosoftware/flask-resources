117e04aa8b04e21fa831f2ec5fa64d7b6dd25957
Pablo Panero <pablo.panero@cern.ch>
2020-11-02 09:55:09 +0100

    global: bump test dependencies and fix build

--- run-tests.sh
+++ run-tests.sh
@@ -12,9 +12,7 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
-pydocstyle flask_resources tests docs && \
-isort flask_resources tests --multi-line=3 --line-width=88 --trailing-comma --force-grid-wrap=0 --use-parentheses --check-only --diff && \
-black --check --diff flask_resources tests && \
-check-manifest --ignore ".*-requirements.txt" && \
-sphinx-build -qnNW docs docs/_build/html && \
+black --check --diff flask_resources tests
+check-manifest --ignore ".*-requirements.txt"
+sphinx-build -qnNW docs docs/_build/html
 pytest
