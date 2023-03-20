7bd49f3d1dd9d42512bbf289912d586caa918d52
Manuel Alejandro <mumox7@gmail.com>
2023-03-20 16:47:25 +0100

    serializers: instantiate classes on init rather than on call
    co-authored-by:  Manuel Alejandro <mumox7@gmail.com>

--- flask_resources/serializers/base.py
+++ flask_resources/serializers/base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020-2021 CERN.
+# Copyright (C) 2020-2023 CERN.
 # Copyright (C) 2020-2021 Northwestern University.
 #
 # Flask-Resources is free software; you can redistribute it and/or modify it
@@ -38,6 +38,7 @@ class MarshmallowSerializer(BaseSerializer):
     :param object_schema_cls: Marshmallow Schema of the object.
     :param list_schema_cls: Marshmallow Schema of the object list.
     :param schema_context: Context of the Marshmallow Schema.
+    :param schema_kwargs: Additional arguments to be passed to marshmallow schema.
     """
 
     def __init__(
@@ -46,38 +47,40 @@ class MarshmallowSerializer(BaseSerializer):
         object_schema_cls,
         list_schema_cls=None,
         schema_context=None,
+        schema_kwargs=None,
         **serializer_options
     ):
         """Initialize the serializer."""
         self.schema_context = schema_context or {}
-        self.object_schema_cls = object_schema_cls
-        self.list_schema_cls = list_schema_cls
-        self.format_serializer_cls = format_serializer_cls(**serializer_options)
+        schema_kwargs = schema_kwargs or {}
+
+        self.format_serializer = format_serializer_cls(**serializer_options)
+        self.object_schema = object_schema_cls(context=schema_context, **schema_kwargs)
+        if list_schema_cls:
+            self.list_schema = list_schema_cls(
+                context={
+                    "object_schema_cls": self.object_schema.__class__,
+                    **self.schema_context,
+                }
+            )
+        else:
+            self.list_schema = None
 
     def dump_obj(self, obj):
         """Dump the object using object schema class."""
-        return self.object_schema_cls(context=self.schema_context).dump(obj)
+        return self.object_schema.dump(obj)
 
     def dump_list(self, obj_list):
         """Dump the list of objects."""
-        ctx = {
-            "object_schema_cls": self.object_schema_cls,
-        }
-        ctx.update(self.schema_context)
+        if not self.list_schema:
+            return self.object_schema.dump(obj_list, many=True)
 
-        if self.list_schema_cls is None:
-            return self.object_schema_cls(context=self.schema_context).dump(
-                obj_list, many=True
-            )
-
-        return self.list_schema_cls(context=ctx).dump(obj_list)
+        return self.list_schema.dump(obj_list)
 
     def serialize_object(self, obj):
         """Dump the object using the serializer."""
-        return self.format_serializer_cls.serialize_object(self.dump_obj(obj))
+        return self.format_serializer.serialize_object(self.dump_obj(obj))
 
     def serialize_object_list(self, obj_list):
         """Dump the object list using the serializer."""
-        return self.format_serializer_cls.serialize_object_list(
-            self.dump_list(obj_list)
-        )
+        return self.format_serializer.serialize_object_list(self.dump_list(obj_list))
