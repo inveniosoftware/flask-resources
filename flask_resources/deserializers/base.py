32e3ec824d95483724769361615b7cfcca33082a
Pablo Panero <pablo.panero@cern.ch>
2023-04-04 17:20:21 +0200

    serializers: add marshmallow schema processors

--- flask_resources/deserializers/base.py
+++ flask_resources/deserializers/base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020-2021 CERN.
+# Copyright (C) 2020-2023 CERN.
 # Copyright (C) 2020-2021 Northwestern University.
 #
 # Flask-Resources is free software; you can redistribute it and/or modify it
@@ -15,3 +15,29 @@ class DeserializerMixin:
     def deserialize(self, data):
         """Deserializes the data into an object."""
         raise NotImplementedError()
+
+
+# this class is not used since there is no MarshmallowDeserializer/Deformatter/Loader
+class LoaderMixin:
+    """Abstract class that defines an interface for pre_load and post_load methods.
+
+    This allows the extension of records deserialization.
+    """
+
+    def post_load(data, **kwargs):
+        """Hook called after the marshmallow deserialization of the record.
+
+        :param data: The loaded record data, already deserialized.
+        :param kwargs: Additional keyword arguments.
+        :returns: The deserialized record data.
+        """
+        return data
+
+    def pre_load(data, **kwargs):
+        """Hook called before the marshmallow deserialization of the record.
+
+        :param data: The record data before being deserialized.
+        :param kwargs: Additional keyword arguments.
+        :returns: The record data.
+        """
+        return data
